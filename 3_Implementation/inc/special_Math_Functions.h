a460e09046151dd79cb08558853d5181c3871639
99003734 <78858575+99003734@users.noreply.github.com>
2021-02-14 16:54:31 +0530

    Created special_Math_Functions.h file

---
+++ 3_Implementation/inc/special_Math_Functions.h
@@ -0,0 +1,11 @@
+# ifndef __math_Functions_H
+# define __math_Functions_H
+#include<stdio.h>
+
+void math_Functions();
+double logarithm(double);
+double exponent(float);
+double squareroot(double);
+double trigonometry(double);
+
+#endif
