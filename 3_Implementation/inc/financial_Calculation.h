f5468fa707b0b5ae9a8682a6fb2748d3e1b3fb40
99003734 <78858575+99003734@users.noreply.github.com>
2021-02-14 16:32:21 +0530

    created financial_Calculation.h file

---
+++ 3_Implementation/inc/financial_Calculation.h
@@ -0,0 +1,12 @@
+# ifndef __FINANCE_CALCULATION_H
+# define __FINANCE_CALCULATION_H
+
+#include<stdio.h> 
+
+void financial_Calculation();
+int NumberLengthCheck(double);
+double simple_Interest(double,float,float);
+double compound_Interest(double,float,float);
+double emi_Calculator(double,float,float);
+
+# endif 
