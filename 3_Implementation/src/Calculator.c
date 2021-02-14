70607d4500870d555b5ea2391ee8375d3fb3f87d
99003733 <78867415+99003733@users.noreply.github.com>
2021-02-14 22:22:15 +0530

    profit loss and physics operation case added

--- 3_Implementation/src/Calculator.c
+++ 3_Implementation/src/Calculator.c
@@ -11,7 +11,9 @@ void calculator()
         if(mode==1)
         {
             printf("\n\n1) PERMUTATIONS AND COMBINATIONS MODE");
-            printf("\n\n2) MATRIX MODE")
+            printf("\n\n2) MATRIX MODE");
+            printf("\n\n3) PHYSICS MODE");
+            printf("\n\n4) PROFIT LOSS MODE");
             printf("Enter your choice:");
             scanf("%d",&ch);
             switch(ch)
@@ -22,6 +24,12 @@ void calculator()
                 case 2:
                     Matrixoperations();
                     break;
+                case 3:
+                    profit_loss();
+                    break;
+                case 4:
+                    phys_operation();
+                    break;       
                 default:
                     printf("\nError");/*Mode doesn't exist*/
                     break;
