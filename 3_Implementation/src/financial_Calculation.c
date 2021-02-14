4f6267ec3514a48e9cafa3ac55f933845293fe5d
99003734 <78858575+99003734@users.noreply.github.com>
2021-02-14 17:44:26 +0530

    update finance_Calculation.c file

--- 3_Implementation/src/financial_Calculation.c
+++ 3_Implementation/src/financial_Calculation.c
@@ -1,50 +1,79 @@
-#include <financial_Calculation.h>
+#include <finance_Calculation.h>
+#include <stdio.h>
 
-#include<stdio.h>
 #include<math.h>
-
-int main()
-
-{
-
- int select_option;
+void finance_Calculation()
+{    
+    static double result;
+    do
+    {
+    int select_option;
  
- float time_period;
- float rate_of_interest;
- float simple_interest;
- float compound_interest;
- float emi_amount;
+    float time_period;
+    float rate_of_interest;
  
- double principal_amount;
-
- printf("Please enter principal amount(P):\n");
- scanf("%lf",&principal_amount);
- printf("please enter time period(T):\n");
- scanf("%f",&time_period);
- printf("please enter rate of interest(R):\n");
- scanf("%f",&rate_of_interest);
- printf("Please choose only one option for financial operation:\n");
- printf(" 1. simple interest\n 2. compound interest\n 3. emi amount\n");
- scanf("%d",&select_option);
- switch(select_option)
+    double principal_amount;
+    
+     printf("Please choose only one option for financial operation:\n");
+     printf(" 1. simple interest\n 2. compound interest\n 3. emi amount\n");
+     scanf("%d",&select_option);
+     
+     switch(select_option)
  {
     case 1 :
-    simple_interest=principal_amount*time_period*rate_of_interest/100;
-    printf("Simple interest=%.2f",simple_interest);
-    break;
+     printf("Please enter principal amount(P):\n");
+     scanf("%lf",&principal_amount);
+     printf("please enter time period(T):\n");
+     scanf("%f",&time_period);
+     printf("please enter rate of interest(R):\n");
+     scanf("%f",&rate_of_interest);
+     result=simple_Interest(principal_amount,time_period,rate_of_interest);
+     printf("%lf",result);
+     break;
     case 2:
-    compound_interest=principal_amount*pow((1 +rate_of_interest/100),time_period)-principal_amount;
-    printf("Compound Interest=%.2f",compound_interest);
-    break;
+     printf("Please enter principal amount(P):\n");
+     scanf("%lf",& principal_amount);
+     printf("please enter time period(T):\n");
+     scanf("%f",&time_period);
+     printf("please enter rate of interest(R):\n");
+     scanf("%f",&rate_of_interest);
+     result=compound_Interest(principal_amount,time_period,rate_of_interest);
+     printf("%lf",result);
+     
+    
+     break;
     case 3:
-     rate_of_interest = rate_of_interest / (12 * 100);  
-    time_period = time_period * 12;
-    emi_amount = (principal_amount * rate_of_interest * pow(1 + rate_of_interest, time_period)) / (pow(1 + rate_of_interest, time_period) - 1);
-    printf("Emi calculation=%.2f",emi_amount);
+    printf("Please enter principal amount(P):\n");
+     scanf("%lf",&principal_amount);
+     printf("please enter time period(T):\n");
+     scanf("%f",&time_period);
+     printf("please enter rate of interest(R):\n");
+     scanf("%f",&rate_of_interest);
+     result=emi_Calculator(principal_amount,time_period,rate_of_interest);
+     printf("%lf",result);
+     
     break;
     default:
     printf("Invalid input!!! Please choose the correct option\n");
     break;
  }
-
+}while(1);
+}
+double simple_Interest(double principal_amount,float time_period,double rate_of_interest )
+{
+double simp_Interest;
+simp_Interest=principal_amount*time_period*rate_of_interest/100;
+return simp_Interest;
+}
+double compound_Interest(double principal_amount,float time_period,double rate_of_interest )
+{
+double comp_Interest;
+comp_Interest=principal_amount*pow((1 +rate_of_interest/100),time_period)-principal_amount;
+return comp_Interest;
+}
+double emi_Calculator(double principal_amount,float time_period,double rate_of_interest )
+{
+double emi_Calc;
+emi_Calc=(principal_amount * rate_of_interest * pow(1 + rate_of_interest, time_period)) / (pow(1 + rate_of_interest, time_period) - 1);
+return emi_Calc;
 }
