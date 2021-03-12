8ab76c4c0508e1148fffa2a070a1b9a1280b955f
99003731 <shivanshu.shukla@ltts.com>
2021-03-12 14:19:34 +0530

    Updated Calculator.c

--- 3_Implementation/src/Calculator.c
+++ 3_Implementation/src/Calculator.c
@@ -2,17 +2,26 @@
 # include"Combinatorics.h"
 # include"Arithmetic.h"
 # include"Matrix.h"
+# include"physics.h"
+# include"profitloss.h"
 
 void calculator()
 {
-    
-          
-                    //combinatorics();
-                   // profit_loss();
-double acceleration(float, float, float);
-double capacitive_rec(float, float);
-double circular_velocity(double, double);
-double gravitation_force(float, float, float);
-double projectile_motion(float , float);
-            //basic_arithmetic();
+    double cal_profit(float, float);
+    double discount(float, float);   
+    double acceleration(float, float, float);
+    double capacitive_rec(float, float);
+    double circular_velocity(double, double);
+    double gravitation_force(float, float, float);
+    double projectile_motion(float , float);
+
+    double Addition(double,double);/*give all function prototype*/
+    double subtract(double,double);
+    double multiply(double,double);
+    double divide(double,double);
+
+    double permutations(int,int);
+    double combinations(int,int);
+
+
 }
